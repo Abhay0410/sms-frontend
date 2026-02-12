ae14e30a9503fe957fdca57345798226e04a3f9d
Abhay0410 <kumarsinghabhay77@gmail.com>
2026-02-12 13:54:05 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,24 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
+  plugins: [react(), tailwindcss()],
+  build: {
+    // Warning limit ko 3MB kar diya taaki yellow warning na aaye
+    chunkSizeWarningLimit: 3000, 
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // React core libraries ko alag file mein rakho
+          'vendor-react': ['react', 'react-dom', 'react-router-dom'],
+          
+          // Heavy libraries ko alag chunks mein todo
+          'vendor-icons': ['react-icons'],
+          'vendor-charts': ['recharts'],
+          'vendor-ui': ['sweetalert2', 'react-toastify', 'react-datepicker'],
+          'vendor-utils': ['date-fns', 'jspdf', 'html2canvas']
+        }
+      }
+    }
+  }
 })
